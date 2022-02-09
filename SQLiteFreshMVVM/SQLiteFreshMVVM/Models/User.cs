d631317e75ae17bf03b73ecf3628e10cfd265481
camilo <casfisica@gmail.com>
2022-02-09 15:35:48 -0500

    SQLiteFressMVVM

---
+++ SQLiteFreshMVVM/SQLiteFreshMVVM/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+
+namespace SQLiteFreshMVVM.Models
+{
+    /// <summary>
+    /// Esta clase contiene los usuarios, roles y password
+    /// </summary>
+
+    [Table(nameof(User))]
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string userName { get; set; }
+        public string name { get; set; }
+        [MaxLength(12)]
+        public string password { get; set; }
+        [MaxLength(10)]
+        public string phone { get; set; }
+        public User()
+        {
+        }
+        /// <summary>
+        /// Para chequear que el usuario esta bien
+        /// </summary>
+        /// <returns></returns>
+        public bool IsValid()
+        {
+            return (!string.IsNullOrWhiteSpace(userName) && !string.IsNullOrWhiteSpace(password));
+        }
+
+    }
+}
